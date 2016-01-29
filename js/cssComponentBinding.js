527d62be2f4552f202de5d1261ecc35dea6a25f7
Josh White <josh@joshdevbox.com>
2016-01-29 21:42:25 +0000

    - Refactored component factory - it now makes sense. - Added media query support

--- js/cssComponentBinding.js
+++ js/cssComponentBinding.js
@@ -1,111 +1,188 @@
 ko.bindingHandlers.cssComponent = {
     init: function (element, valueAccessor) {
-        var self = ko.bindingHandlers.cssComponent;
-
-        // get style with type="text/template" to prevent DOM remdering it
-        var style = $(element).find('style[type="text/template"]').html();
+        var self = ko.bindingHandlers.cssComponent,
+            // get style with type="text/template" to prevent DOM remdering it
+            style = $(element).find('style[type="text/template"]').html(),
+            componentLabel, component;
 
         if (style === undefined) {
             throw 'No style tag with type "text/template" found';
         }
 
-        // create component and return unique hash
-        var hash = self.componentFactory().createComponent(style, valueAccessor());
+        // default name to __component__
+        componentLabel  = typeof valueAccessor() === 'string' ? '__' + valueAccessor() + '__' : '__component__';
 
-        // add "_root" & hash as class 
-        element.className = element.className + ' ' + hash;
+        // create component
+        component = self.componentFactory(style, componentLabel);
 
-        // if component doesn't exist, create, else increment count
-        if (self.components[hash].count === 0) {
-            self.componentFactory().addComponentStyleToHead(hash);
-        }
-        else {
-            self.components[hash].count++;
-        }
+        // add hash to element as class 
+        element.className = element.className + ' ' + component.hash;
 
-        // create data attr with unique hash
-        element.setAttribute('data-component-style', hash);
+        // add to head unless it already exists
+        component.addComponentStyleToHead();
 
-        // remove style blocks
+        // remove style blocks from element
         $(element).find('style').remove();
 
         ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-
-            var hash = element.getAttribute('data-component-style');
-            var component = self.components[hash];
-            component.count--;
-
-            if (component.count === 0) {
-                component.styleBlock.parentNode.removeChild(component.styleBlock);
-            }
+            // remove from head if needed
+            component.removeComponentStyleFromHead();
         });
 
     },
 
-    components: {},
+    componentStore: {},
 
-    componentFactory: function () {
-        var components = ko.bindingHandlers.cssComponent.components;
+    componentFactory: function (css, label) {
+        var componentStore = ko.bindingHandlers.cssComponent.componentStore,
+            hash;
 
+        // if needed, create component & add it to the store
+        createComponent(css, label);
+
+        // returns unique hash & methods for adding and removing components to the DOC head
         return {
-            createComponent: createComponent,
+            hash: hash,
             addComponentStyleToHead: addComponentStyleToHead,
-            generateStringhashCode: generateStringhashCode
+            removeComponentStyleFromHead: removeComponentStyleFromHead
         }
 
-        function createComponent(css, name) {
-             //Removes newlines & comments
-            var parsedCss = css.replace(/(\r\n|\n|\r)|(\/\*(\n|.)+?\*\/|\/\/.*(?=[\n\r]))/gm, "");
+        /* Public functions 
+        ------------------------------------------ */
+        function addComponentStyleToHead() {
+            var component = componentStore[hash],
+                css = component.css,
+                head, styleBlock;
 
-            var componentName = typeof name === 'string' ? '__' + name + '__' : '__component__';
+            if(component.count === 0) {
+
+                // adapted from http://stackoverflow.com/a/524721
+                head = document.head || document.getElementsByTagName('head')[0];
+                styleBlock = document.createElement('style');
+
+                styleBlock.type = 'text/css';
+
+                if (styleBlock.styleSheet) {
+                    styleBlock.styleSheet.cssText = css;
+                }
+                else {
+                    styleBlock.appendChild(document.createTextNode(css));
+                }
+
+                head.appendChild(styleBlock);
+                //
+
+                // Update component values
+                component.count = 1;
+                component.styleBlock = styleBlock;
+            }
+            else {
+                component.count++;
+            }
+        }
+
+        function removeComponentStyleFromHead() {
+            var componentToRemove = componentStore[hash];
+            componentToRemove.count--;
+
+            if (componentToRemove.count === 0) {
+                componentToRemove.styleBlock.parentNode.removeChild(componentToRemove.styleBlock);
+            }
+        }
+
+        /* Private functions 
+        ------------------------------------------ */
+        function createComponent(css, label) {
+             //Removes newlines & comments
+            var parsedCss = css.replace(/(\r\n|\n|\r)|(\/\*(\n|.)+?\*\/|\/\/.*(?=[\n\r]))/gm, ""),
+                mediaQueries = [],
+                noMediaQueryCss, styleBlock;
 
             //create unique component hash with with prefix
-            var hash = componentName + this.generateStringhashCode(parsedCss);
+            hash = label + generateStringhashCode(parsedCss);
 
-            // if component doesn't already exist, add it to components object
-            if (components[hash] === undefined) {
+            // if component doesn't already exist, create & add it to componentStore object
+            if (componentStore[hash] !== undefined) {
+                return;
+            }
+            else {
 
-                //split into array of CSS rules (won't work with media queries)
-                var queries = parsedCss.split(/(?=@media)/);
+                //split into array of CSS styleBlock (won't work with media queries)
+                // var queries = parsedCss.split(/(?=@media)/);
 
-                var newQueries = queries.map(function(line) {
-                	if(line.match(/@media/)) {
-                		var querySelector = line.split(/(?={)/).shift();
-                		var queryRules = line
-                							.split(/(?:{)([\s\S]*)(?:})/)[1]
-                							.split('}')
-                							.map(mappingFunction)
-                							.join('');
+                // var newQueries = queries.map(function(line) {
+                // 	if(line.match(/@media/)) {
+                // 		var querySelector = line.split(/(?={)/).shift();
+                // 		var queryRules = line
+                // 							.split(/(?:{)([\s\S]*)(?:})/)[1]
+                // 							.split('}')
+                // 							.map(mappingFunction)
+                // 							.join('');
 
-                		return querySelector + '{' + queryRules + '}';
+                // 		return querySelector + '{' + queryRules + '}';
 
-                	} 
-                	else {
-                		return line;
-                	}
-                }).join('');
+                // 	} 
+                // 	else {
+                // 		return line;
+                // 	}
+                // }).join('');
 
-                debugger;
+                noMediaQueryCss = extractMediaQueries(parsedCss, mediaQueries);
 
-				var cssRulesArray = parsedCss.split('}');
+                styleBlock = mapCss(noMediaQueryCss, mediaQueries);
 
-                // add hash to each selector in rule 
-                // returns array of strings containing the rules + selector
-                var componentRules = cssRulesArray.map(mappingFunction);
+                addComponentToStore(hash, styleBlock);
 
-                function mappingFunction(cssLine) {
-                    var trimmedCssLine = cssLine.trim();
-                    if(trimmedCssLine.match(/@media/)) {
-                    	return cssLine + '}}';
+            }
+        }
+
+        function extractMediaQueries(css, mediaQueries) {
+            return css
+                //split by media queries
+                .split(/(@media[^{]*{(?:(?!}\s*}).)*}.*?})/)
+                .map(function(line) {
+                    var querySelector, queryRules;
+
+                    // add mappedCss inside queries to containing array
+                    // replace with placeholder
+                    if(line.match(/@media/)) {
+                        querySelector = line.split(/(?={)/).shift();
+                        queryRules = mapCss(line.split(/@media[^{]+\{([\s\S]+?\})\s*\}/)[1]);
+
+                        mediaQueries.push(querySelector + '{' + queryRules + '}');
+
+                        return 'PLACEHOLDER }';
+
+                     } 
+                     // return line without modifying
+                     else {
+                         return line;
+                     }
+                })
+                .join('');
+        }
+
+        function mapCss(css, mediaQueries) {
+            // add hash to each selector in rule 
+            // return component style block
+            return css
+                .split('}')
+                .map(function(cssLine) {
+                    var trimmedCssLine = cssLine.trim(),
+                        selectors;
+
+                    if(trimmedCssLine.match('PLACEHOLDER')) {
+                        return mediaQueries.shift();    
                     }
+
                     if (trimmedCssLine !== "") {
-						var selectors = trimmedCssLine.split('{')[0].trim();
-                        var rule = trimmedCssLine.split('{')[1].trim();
-                        var prependedSelectors = selectors
-							.trim()
-							.split(',')
-							.map(function (selector) {                                
-						        // special selector for targetting the root element
+                        selectors = trimmedCssLine.split('{')[0].trim(),
+                        rule = trimmedCssLine.split('{')[1].trim(),
+                        prependedSelectors = selectors
+                            .trim()
+                            .split(',')
+                            .map(function (selector) {                                
+                                // special selector for targetting the root element
                                 if(selector.trim() === "._root") {
                                     return '.' + hash;  
                                 }
@@ -113,48 +190,23 @@ ko.bindingHandlers.cssComponent = {
                                 else {
                                     return '.' + hash + ' ' + selector.trim();
                                 }
-							});
+                            });
 
                         return prependedSelectors.join(',') + '{' + rule + '}';
                     }
-                }
-
-                //Add to components object
-                components[hash] = {
-                    styleBlock: null,
-                    count: 0,
-                    css: componentRules.join('')
-                };
-
-            }
-
-            // returns component hash
-            return hash;
+                })
+                .join('');
         }
 
-        function addComponentStyleToHead(hash) {
-            var component = components[hash];
-            var css = component.css;
+        // should only be called if component doesn't exist.
+        function addComponentToStore(name, rules) {
+            componentStore[name] = {
+                styleBlock: null,
+                count: 0,
+                css: rules
+            };
 
-            // adapted from http://stackoverflow.com/a/524721
-            var head = document.head || document.getElementsByTagName('head')[0];
-            var styleBlock = document.createElement('style');
-
-            styleBlock.type = 'text/css';
-
-            if (styleBlock.styleSheet) {
-                styleBlock.styleSheet.cssText = css;
-            }
-            else {
-                styleBlock.appendChild(document.createTextNode(css));
-            }
-
-            head.appendChild(styleBlock);
-            //
-
-            // Update component values
-            component.count = 1;
-            component.styleBlock = styleBlock;
+            return componentStore;
         }
 
         // taken from: http://stackoverflow.com/a/7616484
